0fd4ca140672fb764ebae4f54ca46fedefcff0c6
mikesligo <mikesligo@gmail.com>
2012-07-11 15:20:42 +0100

    impl script fully works with threading, reasonably quickly

--- ie.tcd.gmf.visGrid/scripts/impl/runGenerateImpl.sh
+++ ie.tcd.gmf.visGrid/scripts/impl/runGenerateImpl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 connection='../../src/visGrid/impl/ConnectionImpl.java'
-for i in ../../src/visGrid/impl/*
+for i in ../../src/visGrid/impl/*.java
 do
     if [ $i != $connection ];
     then 
